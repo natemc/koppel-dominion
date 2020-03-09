d280e9413fbdf33ab7ff11c4e5d354e1f2ae3a9d
Nate McNamara <nate@mcnamara.net>
2020-03-08 20:09:16 -0400

    make card printing more consistent

--- card.cpp
+++ card.cpp
@@ -1,9 +1,12 @@
 #include <card.h>
+#include <deck.h>
 #include <each.h>
 #include <filter.h>
 #include <iostream>
 #include <k.h>
 #include <lambda.h>
+#include <print_card.h>
+#include <random.h>
 #include <til.h>
 #include <utility>
 
@@ -25,8 +28,15 @@ bool Card::is(Tag t) const {
 }
 
 std::ostream& operator<<(std::ostream& os, const Card& c) {
-    os << c.name << " C" << c.cost << "|T" << c.treasure_points << ' ';
-    for (Tag t: filter(L1(c.is(Tag(x))), each(L1(Tag(x)), til(END_TAGS))))
-        os << t;
-    return os;
+    // print_card requires a Deck (which requires a Random, which requires a
+    // standard-conforming random engine) because of the Gardens card.
+
+    static struct UnrandomEngine {
+        using result_type = std::size_t;
+        result_type operator()() { return 42; }
+        static constexpr result_type max() { return 42; }
+        static constexpr result_type min() { return 42; }
+    } not_random;
+    static Random bogus{not_random};
+    return print_card(os, c, Deck{&bogus});
 }
