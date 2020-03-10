10a4676d5ba632f55c1c04f79535658e5b9ecd74
Nate McNamara <nate@mcnamara.net>
2020-03-09 21:33:22 -0400

    add LNC* (no capture) lambda macros for file-scope lambdas

--- lambda.h
+++ lambda.h
@@ -7,3 +7,8 @@
 #define L0(e) [&](                  ){ return e; }
 #define L1(e) [&](auto&& x          ){ return e; }
 #define L2(e) [&](auto&& x, auto&& y){ return e; }
+
+// No-capture versions for use at file scope
+#define LNC0(e) [](                  ){ return e; }
+#define LNC1(e) [](auto&& x          ){ return e; }
+#define LNC2(e) [](auto&& x, auto&& y){ return e; }
