b7da31dcc661f80b3369845743516566182ba49a
Nate McNamara <nate@mcnamara.net>
2020-03-15 08:44:03 -0400

    add initializer_list support to join

--- join.h
+++ join.h
@@ -6,23 +6,34 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #include <functional>
+#include <initializer_list>
 #include <iterator>
 #include <numeric>
 #include <sstream>
 #include <string>
+#include <utility>
 
 const struct Join {
     template <class C>
     std::string operator()(std::string delim, const C& c) const {
+        return (*this)(std::move(delim), std::begin(c), std::end(c));
+    }
+
+    template <class X>
+    std::string operator()(std::string delim, std::initializer_list<X> x) const {
+        return (*this)(std::move(delim), std::begin(x), std::end(x));
+    }
+
+    template <class I>
+    std::string operator()(std::string delim, I first, I last) const {
         std::ostringstream os;
-        auto it = std::begin(c);
-        if (it != std::end(c)) {
-            os << *it;
-            accumulate(++it, std::end(c), std::ref(os), 
-                       [=](std::ostringstream& acc, auto& x) {
-                           acc << delim << x;
-                           return std::ref(acc);
-                       });
+        if (first != last) {
+            os << *first;
+            std::accumulate(++first, last, std::ref(os), 
+                            [=](std::ostringstream& acc, auto& x) {
+                                acc << delim << x;
+                                return std::ref(acc);
+                            });
         }
         return os.str();
     }
